ba6621b7c99045ae4cb7172f3804eb50e363fca2
matheus <mmartins.oliveira@outlook.com>
2021-06-08 08:12:08 -0300

    feat(styles): add global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -15,8 +15,15 @@ const GlobalStyles = createGlobalStyle`
     height: 100%;
   }
 
+  a {
+    list-style: none;
+    text-decoration: none;
+  }
+
   body {
-    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    background:  #171717;
+    color: #fff;
   }
 `
 
