bee4b1aa6c4c045f2d426d8c00f68509c422ca79
matheus martins <mathews_2010@outlook.com>
2022-07-08 18:50:55 -0300

    fix: add return

--- src/components/ShortcutHome.js
+++ src/components/ShortcutHome.js
@@ -1,37 +1,39 @@
 import { useState, useEffect } from 'react'
 import { useKBar } from 'kbar'
-import { ButtonPrimary } from './ButtonPrimary'
+import { ButtonPrimary } from '../components/ButtonPrimary'
 
 export default function ShortcutHome() {
-    const { query } = useKBar()
-    const [mounted, setMounted] = useState(false)
+  const { query } = useKBar()
+  const [mounted, setMounted] = useState(false)
 
-    useEffect(() => {
-      setMounted(true)
-    }, [])
+  useEffect(() => {
+    setMounted(true)
+  }, [])
 
-    if(mounted) {
-        const isMac = /(Mac)/i.test(navigator.userAgent)
-        const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent)
+  if (mounted) {
+    const isMac = /(Mac)/i.test(navigator.userAgent)
+    const isMobile = /iPhone|iPad|Android/i.test(navigator.userAgent)
 
-        if(isMobile) {
-            return (
-                <ButtonPrimary as="button" onclick={query.toggle}>
-                    Tap to start →
-                </ButtonPrimary>
-            )
-        } else if (isMac) {
-            return (
-                <ButtonPrimary as="button" onclick={query.toggle}>
-                  Press <kbd>⌘</kbd> <kbd>K</kbd> to start →
-                </ButtonPrimary>
-            )
-        } else {
-            return (
-                <ButtonPrimary as="button" onClick={query.toggle}>
-                  Press <kbd>ctrl</kbd> <kbd>K</kbd> to start →
-                </ButtonPrimary>
-            )
-        }
+    if (isMobile) {
+      return (
+        <ButtonPrimary as="button" onClick={query.toggle}>
+          Tap to start →
+        </ButtonPrimary>
+      )
+    } else if (isMac) {
+      return (
+        <ButtonPrimary as="button" onClick={query.toggle}>
+          Press <kbd>⌘</kbd> <kbd>K</kbd> to start →
+        </ButtonPrimary>
+      )
+    } else {
+      return (
+        <ButtonPrimary as="button" onClick={query.toggle}>
+          Press <kbd>ctrl</kbd> <kbd>K</kbd> to start →
+        </ButtonPrimary>
+      )
     }
+  }
+
+  return <div />
 }
